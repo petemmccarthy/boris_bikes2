f5437c08451a10208d69e9ac5011c990d76814d3
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-10-21 13:30:47 +0100

    Added bike spec

---
+++ spec/bike_spec.rb
@@ -0,0 +1,22 @@
+require 'bike'
+
+describe Bike do
+
+  let(:bike)        { Bike.new }
+  let(:broken_bike) { Bike.new.break!}
+  
+  it 'cant be broken when initialized' do
+    expect(bike.broken?).to be false
+  end
+
+  it 'can be broken' do
+    # bike.break!
+    expect(bike.break!).to be_broken
+  end
+
+  it 'can be fixed' do
+    broken_bike.fix!
+    expect(broken_bike.broken?).to be false
+  end
+
+end
